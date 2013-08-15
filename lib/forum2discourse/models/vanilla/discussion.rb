7ea116427d084639ec3ac517d7c384c399b1b427
Mark McDowall <markus.mcd5@gmail.com>
2013-08-14 20:33:57 -0700

    Create new comment from discussion body

--- lib/forum2discourse/models/vanilla/discussion.rb
+++ lib/forum2discourse/models/vanilla/discussion.rb
@@ -7,12 +7,24 @@ class Forum2Discourse::Models::Vanilla::Discussion
   property :created_at,   DateTime, field: 'DateInserted'
   property :category_id,  Integer, field: 'CategoryID'
   property :subject,      Text, field: 'Name'
+  property :body,         Text, field: 'Body'
+  property :user_id       Text, field: 'InsertUserId'
 
   has n, :comments, 'Forum2Discourse::Models::Vanilla::Comment'
   belongs_to :category, 'Forum2Discourse::Models::Vanilla::Category'
 
   def to_discourse
     return nil if category.nil?
+
+    bodyComment = Forum2Discourse::Models::Vanilla::Comment.new
+
+    bodyComment.created_at = created_at
+    bodyComment.discussion_id = id
+    bodyComment.user_id = user_id
+    bodyComment.body = body
+
+    comments.push(bodyComment)
+
     Forum2Discourse::Models::Discourse::Topic.new({
       title: subject,
       created_at: created_at,
